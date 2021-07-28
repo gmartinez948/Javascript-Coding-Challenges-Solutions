ef953f199d9e03c69631ff59e363314292f02fe3
Guillermo Martinez <77513632+gmartinez948@users.noreply.github.com>
2021-07-28 00:45:04 +0000

    Added more challenges

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Write a function that converts an object into an array of keys and values.
+0       // Write a function that converts an object into an array of keys and values.
 
 function objectToArray(obj){
   // create a result array
@@ -19,11 +19,11 @@ function assertEquals(actual, expected, testName) {
     console.log('FAILED [' + testName + '] Expected ' + expected + ', but got ' + actual)
   }
 }
-var actualObj = objectToArray({
-  D: 1,
-  B: 2,
-  C: 3
-});
+// var actualObj = objectToArray({
+//   D: 1,
+//   B: 2,
+//   C: 3
+// });
 // assertEquals(actualObj, [["D", 1], ["B", 2], ["C", 3]], 'Should return an array of arrays with the key and value of obj as a pair')
 
 //////////////////////////////////////////////////////////////////
@@ -50,15 +50,15 @@ function maximumScore(arr) {
 
 
 
-actualScore = maximumScore([
-  { tile: "N", score: 1 },
-  { tile: "K", score: 5 },
-  { tile: "Z", score: 10 },
-  { tile: "X", score: 8 },
-  { tile: "D", score: 2 },
-  { tile: "A", score: 1 },
-  { tile: "E", score: 1 }
-]) 
+// actualScore = maximumScore([
+//   { tile: "N", score: 1 },
+//   { tile: "K", score: 5 },
+//   { tile: "Z", score: 10 },
+//   { tile: "X", score: 8 },
+//   { tile: "D", score: 2 },
+//   { tile: "A", score: 1 },
+//   { tile: "E", score: 1 }
+// ]) 
 
 // 28
 
@@ -383,10 +383,10 @@ function countOnes(arr) {
   return counter;
 }
 
-countOnes([
-  [1, 0],
-  [0, 0]
-]) //➞ 1
+// countOnes([
+//   [1, 0],
+//   [0, 0]
+// ]) //➞ 1
 
 // countOnes([
 //   [1, 1, 1],
@@ -404,11 +404,23 @@ countOnes([
 
 // Create a function that takes a string and returns a new string with all vowels removed.
 
-function removeVowels(str) {
-}
+// function removeVowels(str) {
+//   // create a collection of vowels;
+//   var vowels = 'aeiou'
+//   // convert the input string to an array
+//   var noVowels = [];
+//   // check through the array's elements
+//   for (var i = 0; i < vowels.length; i++) {
+//     console.log(vowels[i])
+//     for (var j = 0;  j < str.length; j++) {
+//     }
+//   }
+//   // return the array in a string format
+//   return noVowels;
+// }
 
-removeVowels("I have never seen a thin person drinking Diet Coke.")
-//➞ " hv nvr sn  thn prsn drnkng Dt Ck."
+// removeVowels("I have never seen a thin person drinking Diet Coke.")
+// //➞ " hv nvr sn  thn prsn drnkng Dt Ck."
 
 // removeVowels("We're gonna build a wall!")
 // //➞ "W'r gnn bld  wll!"
@@ -416,6 +428,319 @@ removeVowels("I have never seen a thin person drinking Diet Coke.")
 // removeVowels("Happy Thanksgiving to all--even the haters and losers!")
 // //➞ "Hppy Thnksgvng t ll--vn th htrs nd lsrs!"
 
+/* An isogram is a word that has no duplicate letters. 
+Create a function that takes a string and returns either true 
+or false depending on whether or not it's an "isogram". 
+*/
+
+function isIsogram(str) {
+  str = str.toLowerCase();
+  var isTrue = true;
+  // check each letter against the next letter of the string
+  for (var i = 0; i < str.length; i++) {
+    for (var j = i + 1; j < str.length; j++) {
+      // console.log(str[j])
+      if (str[i] === str[j]) {
+        return !isTrue;
+      }
+    }
+  }
+    // if it matches
+      // return isTrue is false
+  // return isTrue otherwise
+  return isTrue;
+}
+// isIsogram("Algorism") //➞ true
+
+// isIsogram("PasSword") //➞ false
+// // Not case sensitive.
+
+// isIsogram("Consecutive") //➞ false
+
+
+// assertEquals(isIsogram("PasSword"), false, 'Should return true because characters are not repeated')
+// assertEquals(isIsogram("Algorism"), true, 'Should return false because characters are repeated')
+// assertEquals(isIsogram("Consecutive"), false, 'Should return false because characters are repeated')
+
+////////////////////////////////////////////////////////////////////////////////
+
+/* 
+Create a function that returns the frequency distribution of an array. 
+This function should return an object, where the keys are the unique elements 
+and the values are the frequency in which those elements occur.
+*/
+
+function getFrequencies(arr) {
+  // create a result object to contain each char as key & the # it occurs as the values
+  var frequency = {};
+  // check through the input arrays elements
+  for (var i = 0; i < arr.length; i++) {
+    // if the element is not found as a key in result
+    if (frequency[arr[i]] === undefined) {
+      // add it as the key and set its value to 1
+      frequency[arr[i]] = 1;
+    } else {
+    // if it is found,
+      frequency[arr[i]]++
+      // increment the value by 1.
+    }
+  }
+  // return result object
+  return frequency;
+}
+
+
+// getFrequencies(["A", "B", "A", "A", "A"]) //➞ { A: 4, B: 1 }
+
+// getFrequencies([1, 2, 3, 3, 2]) //➞ { "1": 1, "2": 2, "3": 2 }
+
+// getFrequencies([true, false, true, false, false]) //➞ { true: 2, false: 3 }
+
+// getFrequencies([]) //➞ {}
+
+// assertEquals(getFrequencies(["A", "B", "A", "A", "A"]), { A: 4, B: 1 }, 'Should return an object with the number of times an element occurs')
+
+///////////////////////////////////////////////////////////
+
+// Given a string of numbers separated by a comma and space, return the product of the numbers.
+
+function multiplyNums(str) {
+  str = str.split(', ');
+  console.log(str);
+  var product = 1;
+  for (var i = 0 ; i < str.length; i++) {
+    product *= Number(str[i])
+  }
+  return product;
+}
+
+// multiplyNums("2, 3") //➞ 6
+
+// multiplyNums("1, 2, 3, 4") //➞ 24
+
+// multiplyNums("54, 75, 453, 0")// ➞ 0
+
+// multiplyNums("10, -2") //➞ -20
+
+
+/////////////////////////////////////////////////////////
+
+/*
+Given a word, create an object that stores the indexes of each letter in an array.
+
+Make sure the letters are the keys.
+Make sure the letters are symbols.
+Make sure the indexes are stored in an array and those arrays are values.
+*/
+
+function mapLetters(str) {
+  var result = {};
+  for (var i = 0; i < str.length; i++) {
+    if (result[str[i]] === undefined) {
+      result[str[i]] = [i]
+    } else {
+      result[str[i]].push(i)
+    }
+  }
+  return result;
+}
+
+// mapLetters("dodo") //➞ { d: [0, 2], o: [1, 3] }
+
+// mapLetters("froggy") //➞ { f: [0], r: [1], o: [2], g: [3, 4], y: [5] }
+
+// mapLetters("grapes") //➞ { g: [0], r: [1], a: [2], p: [3], e: [4], s: [5] }
+
+//////////////////////////////////////////////////////////////
+
+
+
+// Create a function that takes a string and censors words over four characters with *.
+
+function censor(str) {
+  // create a result string to add characters to 
+  var censoredString = '';
+  // split the words by the space 
+  var words = str.split(' ');
+  // check over the strings words 
+  for (var i = 0; i < words.length; i++) {
+    // create a variable to hold asterisks
+    var asterisks = '';
+    // if the length of the word is greater than 4
+    if (words[i].length > 4) {
+      // find the length of the word
+      for (var j = 0; j < words[i].length; j++) {
+        // add '*' to the variable to the length of the word
+        asterisks += '*'
+      }
+      // replace the word with the asterisks variable
+      words.splice(i, 1, asterisks) 
+    }
+    console.log(words)
+    // if the index of the word is the last index
+    if (i === words.length - 1) {
+      // add each word in succession to the result string
+      censoredString += words[i];
+    } else {
+    // otherwise, 
+      // add each word with a space
+      censoredString += words[i] + ' ';
+    }
+  }
+  // return the result string
+  return censoredString;
+}
+
+// censor("The code is fourty") //➞ "The code is ******"
+
+// censor("Two plus three is five") //➞ "Two plus ***** is five"
+
+// censor("aaaa aaaaa 1234 12345") //➞ "aaaa ***** 1234 *****"
+
+/*
+Create a function that takes a string and 
+replaces the vowels with another character.
+
+a = 1
+e = 2
+i = 3
+o = 4
+u = 5
+*/
+
+function replaceVowel(str) {
+  // create an object with properties and keys that represent the change
+  var obj = {
+    a: 1, 
+    e: 2,
+    i: 3,
+    o: 4,
+    u: 5
+  }
+  // split the input string into an array of characters
+  var word = str.split('');
+  // console.log(word)
+  // check through the elements of the array
+  for (var i = 0; i < word.length; i++) {
+    // if the current element matches the key in the obj
+    if (obj[word[i]]) {
+      // change the current element to the value of the key in the obj
+      word[i] = obj[word[i]];
+    }
+  }
+  // return the array in string format
+  return word.join('');
+}
+
+// replaceVowel("karachi") //➞ "k1r1ch3"
+
+// replaceVowel("chembur") //➞ "ch2mb5r"
+
+// replaceVowel("khandbari") //➞ "kh1ndb1r3"\
+
+//////////////////////////////////////////////////////////////////////
+/* 
+Create a function that takes an array of 
+students and returns an array of their top notes. 
+If the student does not have notes then let's assume their top note is equal to 0.
+*/
+
+function getStudentTopNotes(arr) {
+  var topNotes = [];
+  // console.log(topNotes)
+  for (var i = 0; i < arr.length; i++) {
+    var highestNote = arr[i].notes[0];
+    for (var j = 0; j < arr[i].notes.length; j++) {
+      // console.log(arr[i].notes[j])  
+      var currentNote = arr[i].notes[j];
+      if (currentNote > highestNote) {
+        highestNote = currentNote
+      }
+    }
+    topNotes.push(highestNote)
+  }
+  return topNotes;
+}
+
+// getStudentTopNotes([
+//   {
+//     id: 1,
+//     name: "Jacek",
+//     notes: [5, 3, 4, 2, 5, 5]
+//   },
+//   {
+//     id: 2,
+//     name: "Ewa",
+//     notes: [2, 3, 3, 3, 2, 5]
+//   },
+//   {
+//     id: 3,
+//     name: "Zygmunt",
+//     notes: [2, 2, 4, 4, 3, 3]
+//   }
+// ]) //➞ [5, 5, 4]
+
+////////////////////////////////////////////////////////////////////////////
+
+/*
+The "Reverser" takes a string as input 
+and returns that string in reverse order, with the opposite case.
+*/
+
+function reverse(str) {
+  var reversedStr = '';
+  for (var i = str.length -1; i >= 0; i--) {
+    if (str[i] === str[i].toLowerCase()) {
+      reversedStr += str[i].toUpperCase();
+    } else {
+      reversedStr += str[i].toLowerCase();
+    }
+  }
+  return reversedStr;
+}
+
+
+
+// reverse("Hello World") //➞ "DLROw OLLEh"
+
+// reverse("ReVeRsE") //➞ "eSrEvEr"
+
+// reverse("Radar") //➞ "RADAr"
+
+/////////////////////////////////////////////////////////////////////////
+
+/*
+An array is special if every even index contains an even number and 
+every odd index contains an odd number. 
+Create a function that returns true if an array is special, and false otherwise.
+*/
+
+function isSpecialArray(arr) {
+  var isSpecial = true;
+  for (var i = 0; i < arr.length; i += 2) {
+    // console.log(arr[i])
+    if (arr[i] % 2 !== 0) {
+      return !isSpecial;
+    }
+    for (var j = 1; j < arr.length; j += 2) {
+    // console.log(arr[j])
+      if (arr[j] % 2 !== 1) {
+        return !isSpecial;
+      }
+    }
+  }
+  return isSpecial;
+}
+
+
+// isSpecialArray([2, 7, 4, 9, 6, 1, 6, 3]) //➞ true
+//Even indices: [2, 4, 6, 6]; Odd indices: [7, 9, 1, 3]
+
+// isSpecialArray([2, 7, 9, 1, 6, 1, 6, 3]) //➞ false
+// // Index 2 has an odd number 9.
+
+// isSpecialArray([2, 7, 8, 8, 6, 1, 6, 3]) //➞ false
+// Index 3 has an even number 8.
 
 
 
